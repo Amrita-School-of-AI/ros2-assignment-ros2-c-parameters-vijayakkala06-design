898f9e12ec63a9a1cdd88eed7f8a40d7ad4e04d6
Vijay Kiran Reddy <vijayakkala06@gmail.com>
2026-02-20 21:05:57 +0530

    Fix ROS2 parameters package and remove build artifacts

--- build/ros2_parameters/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vijay-kiran-reddy/ros2_ws/src/ros2-assignment-ros2-c-parameters-vijayakkala06-design
-# Build directory: /home/vijay-kiran-reddy/ros2_ws/src/ros2-assignment-ros2-c-parameters-vijayakkala06-design/build/ros2_parameters
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
